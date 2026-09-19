49977f8d061841c539d8ebd156d24da8f2bfa55b
jabez-abija2399 <jabeznew2399@gmail.com>
2026-09-19 14:45:00 +0300

    style: reorganize styling tokens into styles directory

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from 'react-router-dom';
-// import { Provider } from "react-redux";
-// import store from "./app/store";
+import { Provider } from "react-redux";
+import store from "./app/store";
 import App from "./App";
-import "./index.css";
+import './styles/index.css';
 
 const rootElement = document.getElementById("root");
 
@@ -16,11 +16,11 @@ const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
+    <Provider store={store}>
       <BrowserRouter>
       <App />
     </BrowserRouter>
-   {/* </Provider> */}
+   </Provider>
  </React.StrictMode>,
 );
 
