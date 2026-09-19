2d85a90ea0e9fd4a652d17c10757ac08bfa50981
Esam-jr <esmaelsabir9@gmail.com>
2026-09-19 12:08:15 +0300

    feat: implement book and user management features with core server setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,8 @@ import mongoose from 'mongoose';
 import authRoutes from './routes/auth.routes';
 import { setupSwagger } from './docs/swagger';
 import userRoutes from './routes/user.routes';
-
+import bookRoutes from './routes/books.route';
+import categoryRoutes from './routes/category.route';
 dotenv.config();
 
 const app: Express = express();
@@ -25,7 +26,8 @@ app.get("/", (_req: Request, res: Response) => {
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
-
+app.use('/api/books', bookRoutes);
+app.use('/api/categories', categoryRoutes);
 
 mongoose.connect(process.env.MONGO_URI ?? "mongodb://localhost:27017/library_db")
   .then(() => {
